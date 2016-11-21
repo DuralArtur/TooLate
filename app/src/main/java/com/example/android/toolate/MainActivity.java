d3cf8f16417b5aeb14e5c358cb01d18eab0b5f1c
duralesko <duralesko@gmail.com>
2016-11-21 21:37:31 +0100

    Activities added

--- app/src/main/java/com/example/android/toolate/MainActivity.java
+++ app/src/main/java/com/example/android/toolate/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.android.toolate;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @OnClick(R.id.menu_play)
+    public void menuPlay(){
+        Intent intent = new Intent(this, BetActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        ButterKnife.setDebug(true);
+        ButterKnife.bind(this);
     }
 }
